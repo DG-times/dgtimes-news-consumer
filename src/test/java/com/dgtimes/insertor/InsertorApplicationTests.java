6696e834cb69a2e476445f69b79ae2ca8a8ea94a
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-09-03 12:32:22 +0900

    Build

---
+++ src/test/java/com/dgtimes/insertor/InsertorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgtimes.insertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
